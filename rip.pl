6cade259292e576f4879a024515d70f701bf5637
Phil Cluff <philc@localhost>
2013-06-16 22:18:27 +0100

    Complete logging re-write

--- rip.pl
+++ rip.pl
@@ -12,59 +12,72 @@ my $input_device = '/dev/sr0';
 my @titles_to_transcode_for_this_drive = find_and_filter_titles($input_device, $min_duration_seconds, $max_duration_seconds);
 print Dumper @titles_to_transcode_for_this_drive;
 
-
 sub log_info {
     my ($device, $message) = @_;
     my $now = localtime;
-    print "[$now] - [$device] - $message";
+    print "[INFO] - [$now] - [$device] - $message\n";
+}
+
+sub log_warn {
+    my ($device, $message) = @_;
+    my $now = localtime;
+    print "[WARN] - [$now] - [$device] - $message\n";
+}
+
+sub log_debug {
+    if ($debug) {
+	my ($device, $message) = @_;
+	my $now = localtime;
+	print "[DEBUG] - [$now] - [$device] - $message\n";
+    }
 }
 
 sub get_title_from_raw {
-    my @raw = @_;
+    my ($device, @raw) = @_;
     my @titles = grep(/DVD Title:/, @raw);
-    print "WARNING: Could not find title in output." if scalar @titles < 1;
-    print "WARNING: Found many titles in output." if scalar @titles > 1;
-    print "Title (raw): " . $titles[0] . "\n" if $debug;
+    log_warn($device, "Could not find title in output.") if scalar @titles < 1;
+    log_warn($device, "Found many titles in output.") if scalar @titles > 1;
+    log_debug($device, "Title (raw): " . $titles[0]);
     $titles[0] =~ /DVD Title: (.*?)$/;
     my $title = $1;
-    print "Title (processed): " . $title . "\n" if $debug;
+    log_debug($device, "Title (processed): " . $title);
     return $title;
 }
 
 sub get_serial_from_raw {
-    my @raw = @_;
+    my ($device, @raw) = @_;
     my @serials = grep(/DVD Serial Number:/, @raw);
-    print "WARNING: Could not find serial in output." if scalar @serials < 1;
-    print "WARNING: Found many serials in output." if scalar @serials > 1;
-    print "Serial (raw): " . $serials[0] . "\n" if $debug;
+    log_warn($device, "Could not find serial in output.") if scalar @serials < 1;
+    log_warn($device, "Found many serials in output.") if scalar @serials > 1;
+    log_debug($device, "Serial (raw): " . $serials[0]);
     $serials[0] =~ /DVD Serial Number: (.*?)$/;
     my $serial = $1;
-    print "Serial (processed): " . $serial . "\n" if $debug;
+    log_debug($device, "Serial (processed): " . $serial);
     return $serial;
 }
 
 sub find_and_filter_titles {
     my ($input_device, $min_duration_seconds, $max_duration_seconds) = @_;
 
-    log_info($input_device, "About to scan $input_device...\n");
+    log_info($input_device, "About to scan $input_device...");
     my @scan_return_raw = `HandBrakeCLI -v -i $input_device -t 0 2>&1`;
-    log_info($input_device, "Got " . scalar(@scan_return_raw) . " lines from scan.\n\n");
+    log_info($input_device, "Got " . scalar(@scan_return_raw) . " lines from scan.");
 
-    log_info($input_device, "Chomping results...\n");
+    log_info($input_device, "Chomping results...");
     chomp @scan_return_raw;
-    log_info($input_device, "Chomp done.\n\n");
+    log_info($input_device, "Chomp done.");
 
-    print "Getting Title & Serial from results...\n";
-    my $title = get_title_from_raw(@scan_return_raw);
-    my $serial = get_serial_from_raw(@scan_return_raw);
-    print "Got Serial [$serial] and Title [$title]\n\n";
+    log_info($input_device, "Getting Title & Serial from results...");
+    my $title = get_title_from_raw($input_device, @scan_return_raw);
+    my $serial = get_serial_from_raw($input_device, @scan_return_raw);
+    log_info($input_device, "Got Serial [$serial] and Title [$title]");
 
-    print "Stripping out the non-useful data from dump...\n";
+    log_info($input_device, "Stripping out the non-useful data from dump...");
     my @scan_return_processed = grep(/^\s*\+/, @scan_return_raw);
     my $stripped_lines_count = scalar(@scan_return_raw) - scalar(@scan_return_processed);
-    print "Removed $stripped_lines_count lines of data from the dump.\n\n";
+    log_info($input_device, "Removed $stripped_lines_count lines of data from the dump.");
 
-    print "Processing response from Handbrake into a hash...\n";
+    log_info($input_device, "Processing response from Handbrake into a hash...");
     my %disk_titles;
     my $latest_title_id;
 
@@ -72,7 +85,7 @@ sub find_and_filter_titles {
 
 	# If we've found a "+ title", perform title switching logic. Stash away the title ID so we can use it for upcoming lines.
 	if ($scan_line =~ m/\+ title (\d+)/) {
-	    print "    Found Title! [" . $scan_line . "]\n";
+	    log_info($input_device, "    Found Title! [" . $scan_line . "]");
 	    $disk_titles{$1} = {"title_id" => $1};
 	    $disk_titles{$1}->{"raw_title"} = $scan_line;
 	    $latest_title_id = $1;
@@ -83,28 +96,25 @@ sub find_and_filter_titles {
 	    # Duration
 	    if ($scan_line =~ m/\s*\+ duration: (\d\d):(\d\d):(\d\d)/) {
 		my ($h, $m, $s) = ($1, $2, $3);
-		print "        Found duration! [" . $scan_line . "]\n";
+		log_info($input_device, "        Found duration! [" . $scan_line . "]");
 		$disk_titles{$latest_title_id}->{"duration_seconds"} = ($h * 3600) + ($m * 60) + $s;
 		$disk_titles{$latest_title_id}->{"raw_duration"} = $scan_line;
 	    }
 
 	    else {
-		if ($debug) {
-		    print "Warning: Did not parse line: [" . $scan_line . "]\n";
-		}
+		log_debug($input_device, "Did not parse line: [" . $scan_line . "]");
 	    }
 	}
     }
-    print "Found " . scalar(keys(%disk_titles)) . " titles for transcode.\n\n";
+    log_info($input_device, "Found " . scalar(keys(%disk_titles)) . " titles for transcode.");
 
-    print "Filtering out titles that don't fit within duration windows (Min: $min_duration_seconds | Max: $max_duration_seconds)...\n";
+    log_info($input_device, "Filtering out titles that don't fit within duration windows (Min: $min_duration_seconds | Max: $max_duration_seconds)...");
     my $filtered_count = 0;
     foreach my $title_id (keys(%disk_titles)) {
-	print "    Testing Title: $title_id\n";
+	log_info($input_device, "    Testing Title: $title_id");
 	my $this_title_duration = $disk_titles{$title_id}->{duration_seconds};
 	if (($this_title_duration > $min_duration_seconds) and ($this_title_duration < $max_duration_seconds)) {
-
-	    print "        Will transcode Title: $title_id (Duration: $this_title_duration seconds)\n";
+	    log_info($input_device, "        Will transcode Title: $title_id (Duration: $this_title_duration seconds)");
 	}
 	else {
 	    $filtered_count++;
@@ -112,11 +122,12 @@ sub find_and_filter_titles {
 	}
     }
     my $to_transcode_count = scalar(keys(%disk_titles));
-    print "Filtered out $filtered_count titles. Will transcode $to_transcode_count titles.\n\n";
+    log_info($input_device, "Filtered out $filtered_count titles. Will transcode $to_transcode_count titles.");
 
-    print Dumper \%disk_titles if $debug;
+    # Last chance to see the disk info objects.
+    log_debug($input_device, Dumper \%disk_titles);
 
-    print "Coercing data into a more sensible (And ordered) format and generating TranscodeRequests...\n";
+    log_info($input_device, "Coercing data into a more sensible (And ordered) format and generating TranscodeRequests...");
     my @transcode_requests;
     my @sorted_title_ids = sort {$a <=> $b} (keys(%disk_titles));
     foreach my $title_id (@sorted_title_ids) {
@@ -124,7 +135,7 @@ sub find_and_filter_titles {
 	push @transcode_requests, $transcode_request;
 
     }
-    print "Replying with " . scalar @transcode_requests . " TranscodeRequest objects.\n\n";
+    log_info($input_device, "Requesting " . scalar @transcode_requests . " TranscodeRequest objects.");
     return @transcode_requests;
 
 }
