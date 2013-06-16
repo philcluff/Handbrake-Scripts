044be5cc96f75003c158e502efb6b81cc1661a0c
Phil Cluff <philc@localhost>
2013-06-16 19:26:58 +0100

    Added Disk serial and title extraction

--- rip.pl
+++ rip.pl
@@ -5,19 +5,37 @@ use strict;
 use Data::Dumper;
 
 my @drives = [];
+my $debug = $ENV{DEBUG};
 
 print "About to scan drive...\n";
 my @scan_return_raw = `HandBrakeCLI -v -i /dev/sr0 -t 0 2>&1`;
-print "Scan done! (Got " . scalar(@scan_return_raw) . " lines)\n\n";
+print "Got " . scalar(@scan_return_raw) . " lines from scan.\n\n";
 
 print "Chomping results...\n";
 chomp @scan_return_raw;
-print "Chomp done!\n\n";
+print "Chomp done.\n\n";
+
+print "Getting Title & Serial from results...\n";
+my @titles = grep(/DVD Title:/, @scan_return_raw);
+my @serials = grep(/DVD Serial Number:/, @scan_return_raw);
+print "WARNING: Could not find title in output." if scalar @titles < 1;
+print "WARNING: Found many titles in output." if scalar @titles > 1;
+print "WARNING: Could not find serial in output." if scalar @serials < 1;
+print "WARNING: Found many serials in output." if scalar @serials > 1;
+print "Title (raw): " . $titles[0] . "\n" if $debug;
+$titles[0] =~ /DVD Title: (.*?)$/;
+my $title = $1;
+print "Title (processed): " . $title . "\n" if $debug;
+print "Serial (raw): " . $serials[0] . "\n" if $debug;
+$serials[0] =~ /DVD Serial Number: (.*?)$/;
+my $serial = $1;
+print "Serial (processed): " . $serial . "\n" if $debug;
+print "Got Serial [$serial] and Title [$title]\n\n";
 
 print "Stripping out the non-useful data from dump...\n";
 my @scan_return_processed = grep(/^\s*\+/, @scan_return_raw);
 my $stripped_lines_count = scalar(@scan_return_raw) - scalar(@scan_return_processed);
-print "Stripping done! (Removed $stripped_lines_count)\n\n";
+print "Removed $stripped_lines_count lines of data from the dump.\n\n";
 
 print "Processing response from Handbrake into a hash.\n";
 my %disk_titles;
@@ -44,12 +62,15 @@ foreach my $scan_line (@scan_return_processed) {
 	}
 
 	else {
-	    if ($ENV{DEBUG}) {
+	    if ($debug) {
 		print "Warning: Did not parse line: [" . $scan_line . "]\n";
 	    }
 	}
     }
 }
+print "Found " . scalar(keys(%disk_titles)) . " titles for transcode.\n\n";
+
+print "Filtering out titles that don't fit within duration windows...\n\n";
 
 print "Processing done!\n\n";
 print Dumper \%disk_titles;
